d0512720fd2ee6a66a109e6884ff40651c6fd155
Lupeydev <197533886+Lupeydev@users.noreply.github.com>
2026-02-05 15:10:57 -0600

    CLIMBER IS BETTER

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,140 +1,113 @@
 package frc.robot.subsystems;
 
-
+import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.ResetMode;
+import com.revrobotics.sim.SparkAbsoluteEncoderSim;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkRelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.ClimbSubsystemConstants;
+import frc.robot.Constants.ClimbSubsystemConstants.PivotSetPoints;
 import frc.robot.Configs;
-//import frc.robot.Constants.ElevatorConstants;
-import frc.robot.Constants.climbConstants;
-import frc.robot.Constants.ShooterSubsystemConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
-  private SparkMax m_otor;
+    // Initialize intake Spark. We will use open loop control for this
 
-  private SparkClosedLoopController p_motor;
+    private SparkMax m_pivotMotor =
+        new SparkMax(ClimbSubsystemConstants.kPivotMotorCanId, MotorType.kBrushless);
 
-  private SparkAbsoluteEncoder e_cal;
+    private SparkAbsoluteEncoder ae_pivotMotor;
+    private RelativeEncoder re_pivotMotor;
 
-  private RelativeEncoder e_ncoder;
+    private SparkClosedLoopController p_pivotMotor;
 
-  private SparkMaxConfig c_shepherd;
+    private double m_setpoint;
 
-  private double m_setpoint;
+    public ClimberSubsystem() {
+        /*
+        * Apply the appropriate configurations to the SPARKs.
+        *
+        * kResetSafeParameters is used to get the SPARK to a known state. This
+        * is useful in case the SPARK is replaced.
+        *
+        * kPersistParameters is used to ensure the configuration is not lost when
+        * the SPARK loses power. This is useful for power cycles that may occur
+        * mid-operation.
+        */
 
-  private String s_motorName;
+        m_pivotMotor.configure(
+            Configs.ClimberSubsystem.pivotConfig,
+            ResetMode.kResetSafeParameters,
+            PersistMode.kPersistParameters);
 
-  public boolean ran;
+        m_setpoint = PivotSetPoints.kStartPosition;
 
-   private SparkMax m_climbMotor =
-      new SparkMax(ShooterSubsystemConstants.kFeederMotorCanId, MotorType.kBrushless);
+        p_pivotMotor = m_pivotMotor.getClosedLoopController();
+        
+        re_pivotMotor = m_pivotMotor.getEncoder();
+        ae_pivotMotor = m_pivotMotor.getAbsoluteEncoder();
 
-  public ClimberSubsystem() {
+        re_pivotMotor.setPosition(0);
 
-
-    //c_shepherd = Configs.ElevatorSubsystem.shepherdConfig;
-
-   // m_otor.configure(c_shepherd, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    
-    m_setpoint = climbConstants.setpoint;
-
-    p_motor = m_climbMotor.getClosedLoopController();
-
-    e_ncoder = m_climbMotor.getEncoder();
-
-    e_cal = m_climbMotor.getAbsoluteEncoder();
-
-    //e_ncoder.setPosition(0);
-
-    e_ncoder.setPosition(e_cal.getPosition());
-
-  }
-
-  public boolean atTargetPosition() {
-    return Math.abs(avgEncoderPos() - m_setpoint) < 1;
-  }
-
-  public void setthepos() {
-    while(e_ncoder.getPosition() < climbConstants.finalpoint-5){
-      m_otor.set(climbConstants.k_motorSpeed);
-    }
-    m_otor.set(0);
-  }
-
-  public void resetthepos() {
-    while(e_ncoder.getPosition() > climbConstants.setpoint+5){
-      m_otor.set(-climbConstants.k_motorSpeed);
-    }
-    m_otor.set(0);
+        System.out.println("---> IntakeSubsystem initialized");
     }
 
-  public double avgEncoderPos() {
-    return (e_ncoder.getPosition() / 2);
-  }
+    public boolean atTargetPoint() {
+        return Math.abs(re_pivotMotor.getPosition() - m_setpoint) < PivotSetPoints.kPositionTolerance;
+    }
 
-  public void setTargetPosition(double setpoint) {
-    m_setpoint = setpoint;
-    moveToSetpoint();
-  }
+    public void setTargetPosition(double setpos) {
+        m_setpoint = setpos;
+        moveToSetPoint();
+    }
 
-  private void moveToSetpoint() {
-    p_motor.setReference(m_setpoint, ControlType.kMAXMotionPositionControl);
-  }
+    public void moveToSetPoint() {
+        p_pivotMotor.setSetpoint(m_setpoint, ControlType.kMAXMotionPositionControl);
+    }
 
-  public void stickControl(double stick) {
-    m_otor.set(stick);
-  }
+    /**
+     * {@link Command} to run the intake motor power {@link Command}. When the
+     * {@link Command} is interrupted, e.g. the button is released,
+     * the motors will stop
+     * 
+     * @author Pubert
+     */
+    /**
+     * {@link Command} to move the Pivot Motor forward.
+     * 
+     * @author Pubert
+     */
+    public Command runForwardPivot() {
+        return this.run(
+            () -> setTargetPosition(90.0)
+        )
+        .withName("Moving Pivot Forward");
+    }
 
-  public Command resetClimber() {
-    return run(
-      () -> resetthepos());
-  }
+    /**
+     * {@link Command} to move the Pivot Motor backward.
+     * 
+     * @author Pubert
+     */
+    public Command runBackwardPivot() {
+        return this.run(
+                () -> setTargetPosition(0.0)
+            )
+            .withName("Moving Pivot Backward");
+    }
 
-  public Command slowBottom() {
-    return startEnd(
-      () -> m_otor.set(-0.1),
-      () -> m_otor.set(0)
-      );
-  }
-
-  public Command setClimber() {
-    return run(
-      () -> setthepos());
-  }
-  public void setArmCoastMode(){
-    SparkMaxConfig c_mod = new SparkMaxConfig();
-    c_mod.idleMode(IdleMode.kCoast);
-    m_otor.configure(c_mod, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
-  }
-
-  public void setArmBrakeMode(){
-    SparkMaxConfig c_mod = new SparkMaxConfig();
-    c_mod.idleMode(IdleMode.kBrake);
-    m_otor.configure(c_mod, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
-  }
-  
-  @Override
-  public void periodic() { // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Calibrator Position", e_cal.getPosition());
-    SmartDashboard.putNumber("Calibrator Velocity", e_cal.getVelocity());
-    SmartDashboard.putNumber("Shepherd Position", e_ncoder.getPosition());
-    SmartDashboard.putNumber("Shepherd Velocity", e_ncoder.getVelocity());
-    SmartDashboard.putNumber("Setpoint", m_setpoint);
-    SmartDashboard.putBoolean("At Target", atTargetPosition());
-    SmartDashboard.putNumber(s_motorName + " Output", m_otor.getAppliedOutput());
-    SmartDashboard.putNumber(s_motorName + " Current", m_otor.getOutputCurrent());
-    SmartDashboard.putNumber(s_motorName + " Position", e_ncoder.getPosition());
-    SmartDashboard.putNumber(s_motorName + " Velocity", e_ncoder.getVelocity());
-  }
+    @Override
+    public void periodic() {
+        // Display subsystem values
+        SmartDashboard.putNumber("Pivot | Pivot | Applied Output", m_pivotMotor.getAppliedOutput());
+    }
 }
