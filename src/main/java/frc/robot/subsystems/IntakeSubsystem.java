3794a3145a5bdd6875a95c8fdc7059171a53feb8
Mitch Sr <151555056+MitchRM@users.noreply.github.com>
2026-02-21 10:34:30 -0600

    Comment on pivot motor control mode

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -87,7 +87,7 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void moveToSetPoint() {
-        p_pivotMotor.setSetpoint(m_setpoint, ControlType.kPosition);
+        p_pivotMotor.setSetpoint(m_setpoint, ControlType.kPosition); // USING PID POSITION CONTROL
     }
 
     /**
